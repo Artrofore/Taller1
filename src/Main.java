538c87945d90190022ad33bb915e8dabd3470f5d
javier <j.droguett01@ufromail.cl>
2025-04-10 09:33:55 -0400

    init

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,68 @@
-public class Main {
+import java.util.Scanner;
+
+public class Menu {
+
+    private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        mostrarMenu();
+    }
+
+    public static void mostrarMenu() {
+        System.out.println("----Bienvenido al menú de operaciones de matrices, existen las siguientes opciones----");
+
+        while (true) {
+            System.out.println("\nMenú:");
+            System.out.println("1. Crear una matriz.");
+            System.out.println("2. Llamar matriz.");
+            System.out.println("3. Buscar/mostrar fila.");
+            System.out.println("4. Verificar si la matriz es 0.");
+            System.out.println("5. Salir.");
+
+            int opcion = leerOpcionMenu();
+
+            switch (opcion) {
+                case 1:
+                    System.out.println(">> Opción: Crear una matriz.");
+                    // Lógica para crear matriz
+                    ;
+                    boolean validarDimensiones(int filas, int cols) ;
+                    break;
+                case 2:
+                    System.out.println("Opción: Llamar matriz.");
+                    // Lógica para llamar matriz
+                    ;
+                    break;
+                case 3:
+                    System.out.println("Opción: Buscar o mostrar fila.");
+                    // Lógica para mostrar fila
+
+                    break;
+                case 4:
+                    System.out.println("Opción: Verificar si la matriz es 0.");
+                    // Lógica para verificar matriz nula
+
+                    break;
+                case 5:
+                    System.out.println("Hasta luego!!");
+                    return;
+                default:
+                    System.out.println("Opción no válida.");
+                    break;
+            }
+        }
+    }
+
+    private static int leerOpcionMenu() {
+        while (true) {
+            System.out.print("Seleccione una opción: ");
+            String input = scanner.nextLine();
+
+            if (input.matches("[1-5]")) {
+                return Integer.parseInt(input);
+            } else {
+                System.out.println("Opción no válida, por favor seleccione una opción del menú.");
+            }
+        }
     }
 }
